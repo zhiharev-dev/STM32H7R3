efbd593e24b79579245139aa09a13d04ca8f734c
Maxim <zhiharev.dev@mail.ru>
2026-01-08 23:36:57 +0300

    Инициализировать GPIO XSPI1

--- Boot/Application/core/gpio.c
+++ Boot/Application/core/gpio.c
@@ -36,6 +36,8 @@ gpio_handle_t gpio_led_system = {
 
 static void gpio_led_init(void);
 
+static void gpio_xspi1_init(void);
+
 /* Private user code ------------------------------------------------------- */
 
 /**
@@ -45,8 +47,11 @@ void gpio_init(void)
 {
     HAL_GPIOA_ENABLE_CLOCK();
     HAL_GPIOB_ENABLE_CLOCK();
+    HAL_GPIOO_ENABLE_CLOCK();
+    HAL_GPIOP_ENABLE_CLOCK();
 
     gpio_led_init();
+    gpio_xspi1_init();
 }
 /* ------------------------------------------------------------------------- */
 
@@ -65,5 +70,67 @@ static void gpio_led_init(void)
     hal_gpio_set_state(&gpio_led_system, GPIO_RESET);
 
     hal_gpio_init(&gpio_led_system, &gpio_init);
+}
+/* ------------------------------------------------------------------------- */
+
+/**
+ * @brief Инициализировать GPIO XSPI1
+ */
+static void gpio_xspi1_init(void)
+{
+    /*
+     * GPIOO0 XSPIM_P1_NCS1
+     * GPIOO4 XSPIM_P1_CLK
+     * GPIOP0 XSPIM_P1_IO0
+     * GPIOP1 XSPIM_P1_IO1
+     * GPIOP2 XSPIM_P1_IO2
+     * GPIOP3 XSPIM_P1_IO3
+     */
+
+    gpio_init_t gpio_init = {
+         .mode = GPIO_AF,
+         .otype = GPIO_PUSH_PULL,
+         .ospeed = GPIO_VERY_HIGH_SPEED,
+         .pupd = GPIO_NO_PULL,
+         .af = 9,
+    };
+
+    gpio_handle_t gpio_ncs1 = {
+        .instance = GPIOO,
+        .pin = GPIO_PIN0,
+    };
+
+    gpio_handle_t gpio_clk = {
+        .instance = GPIOO,
+        .pin = GPIO_PIN4,
+    };
+
+    gpio_handle_t gpio_io0 = {
+        .instance = GPIOP,
+        .pin = GPIO_PIN0,
+    };
+
+    gpio_handle_t gpio_io1 = {
+        .instance = GPIOP,
+        .pin = GPIO_PIN1,
+    };
+
+    gpio_handle_t gpio_io2 = {
+        .instance = GPIOP,
+        .pin = GPIO_PIN2,
+    };
+
+    gpio_handle_t gpio_io3 = {
+        .instance = GPIOP,
+        .pin = GPIO_PIN3,
+    };
+
+
+    hal_gpio_init(&gpio_ncs1, &gpio_init);
+    hal_gpio_init(&gpio_clk, &gpio_init);
+    hal_gpio_init(&gpio_io0, &gpio_init);
+    hal_gpio_init(&gpio_io1, &gpio_init);
+    hal_gpio_init(&gpio_io2, &gpio_init);
+    hal_gpio_init(&gpio_io3, &gpio_init);
 }
 /* ------------------------------------------------------------------------- */
